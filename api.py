c73209b4a882f958f7528e0e13064233c6d52c26
kagut57 <156945786+kagut57@users.noreply.github.com>
2025-02-26 19:30:32 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,12 +2,12 @@ from aiohttp import web
 
 
 async def root_handler(request):
-    return web.json_response({"status": "Online"})
+    return web.json_response({"status": "Made with ❤️ By @cant_think_1"})
 
 async def web_server():
     app = web.Application()
     app.add_routes([web.get("/", root_handler)])
     runner = web.AppRunner(app)
     await runner.setup()
-    PORT = 3000
+    PORT = 8000
     await web.TCPSite(runner, "0.0.0.0", PORT).start()
